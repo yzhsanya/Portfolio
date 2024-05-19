c16a9cae1c18765d245df0385742bc143d2148d8
yzhsanya <157041207+yzhsanya@users.noreply.github.com>
2024-05-19 15:17:49 +0100

    Add files via upload

---
+++ WebSite_WebDevProject/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('mercedesWikiButton').onclick = function() {
+    window.location.href = "https://en.wikipedia.org/wiki/Mercedes-Benz_G-Class";//sending to a wiki page when button is clicked
+  };
+  document.getElementById('getStartedButton').onclick = function(){//sending to a history.html page when the button is clicked
+    window.location.href = "history.html"
+  };
